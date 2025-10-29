189f12d4f26d5dc6f7ab6ba8cf084cb34cb5a607
maeb44 <denchik2004@yandex.ru>
2025-10-29 13:12:45 +0300

    feat: add solutiob for lab9

---
+++ lab9/SalesDb/SalesDb/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesDb
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void saleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.saleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.salesDataSet);
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "salesDataSet.Sale". При необходимости она может быть перемещена или удалена.
+            this.saleTableAdapter.Fill(this.salesDataSet.Sale);
+
+        }
+        private void товарыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProductForm f = new ProductForm();
+            f.Show();
+        }
+        private void отчетToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Report f = new Report();
+            f.Show();
+        }
+    }
+}
